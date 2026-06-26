e31267319b2d92181a6108357fcddf9279664873
Norlan Ukwatta <norlanukwatta363@gmail.com>
2026-06-26 16:34:14 +0530

    commit 3

--- src/main/java/lk/jiat/ecom/web/servlet/Test.java
+++ src/main/java/lk/jiat/ecom/web/servlet/Test.java
@@ -1,5 +1,6 @@
 package lk.jiat.ecom.web.servlet;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,16 +8,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.jiat.ecom.user.remote.TestRemote;
 
-import javax.naming.InitialContext;
 import java.io.IOException;
 
-@WebServlet("/test")
+@WebServlet(value = "/test",loadOnStartup = 1)
 public class Test extends HttpServlet {
+
+    @EJB //J2EE 5+
+    private TestRemote testRemote;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Test init");
+    }
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().write("Ecom web module test");
 
 //        try {
+        //J2EE 1+
 //            InitialContext ic = new InitialContext();
 //            UserRemote userRemote = (UserRemote)
 //                    ic.lookup("java:global/ecom-user-1.0/UserSessionBean");
@@ -28,15 +38,17 @@ public class Test extends HttpServlet {
 //            throw new RuntimeException(e);
 //        }
 
-        try {
-            InitialContext ic = new InitialContext();
-            TestRemote tc = (TestRemote)
-                    ic.lookup("java:global/ecom-user-1.0/TestSessionBean");
-            String test = tc.test();
-            resp.getWriter().write(test);
-        } catch (Exception e) {
-            throw new ServletException(e);
-        }
+//        try {
+//            InitialContext ic = new InitialContext();
+//            TestRemote tc = (TestRemote)
+//                    ic.lookup("java:global/ecom-user-1.0/TestSessionBean");
+//            String test = tc.test();
+//            resp.getWriter().write(test);
+//        } catch (Exception e) {
+//            throw new ServletException(e);
+//        }
+
+        testRemote.test();
 
     }
 }
